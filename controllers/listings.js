b07f85a769d5bd1bff9d2aa99b57de3f8d3be859
mdzubairx <zubair01rke@gmail.com>
2026-08-21 23:55:34 +0530

    removed the Maps code completely

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,4 @@
 const Listing = require('../Models/listing.js')
-// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-// const mapToken = process.env.MAP_TOKEN;
-// const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
 module.exports.index =   async(req, res)=>{
